eefbc31f720167b39fc9157192851784ca246f93
Mike Hughes <mike@michaelchughes.com>
2019-01-22 13:57:54 -0500

    FIX updated hw0 to make side effect assumptions clear

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -27,13 +27,21 @@ def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
     -------
     x_train_MF : 2D array, shape = (n_train_examples, n_features) (M, F)
         Each row is a feature vector
+        Should be a separately allocated array, NOT a view of any input array
 
     x_test_NF : 2D array, shape = (n_test_examples, n_features) (N, F)
         Each row is a feature vector
+        Should be a separately allocated array, NOT a view of any input array
+
+    Post Condition
+    --------------
+    This function should be side-effect free. The provided input array x_all_LF
+    should not change at all (not be shuffled, etc.)
 
     Examples
     --------
     >>> x_LF = np.eye(10)
+    >>> xcopy_LF = x_LF.copy() # preserve what input was before the call
     >>> train_MF, test_NF = split_into_train_and_test(
     ...     x_LF, frac_test=0.3, random_state=np.random.RandomState(0))
     >>> train_MF.shape
@@ -52,6 +60,10 @@ def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
     [[0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]
      [1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
      [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]]
+
+    ## Verify that input array did not change due to function call
+    >>> np.allclose(x_LF, xcopy_LF)
+    True
 
     References
     ----------
